496f7b82d0b774d73b01ed31defddcbf2c803226
Lakhan-Nad <lakhannad1999@gmail.com>
2020-04-08 18:47:21 +0530

    add docs

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const server = http.createServer(app);
 
 const port = process.env.PORT || 8080;
 
+console.log("MongoDB URI: ", process.env.MONGO_URI);
+
+console.log("Listening on port ", port);
+
 server.listen(port);
